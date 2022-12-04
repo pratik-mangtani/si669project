53c4d8f7ae441b0acadb9be52600e1d07f0d2e88
Pratik Mangtani <mpratik@umich.edu>
2022-12-04 17:53:18 -0500

    favorites

--- screens/Favourites.js
+++ screens/Favourites.js
@@ -3,8 +3,8 @@ import { StyleSheet, Text, View, FlatList, Image, TouchableOpacity } from 'react
 import { useSelector, useDispatch } from 'react-redux';
 import { Card, Button, Icon } from '@rneui/themed';
 import AsyncStorage from '@react-native-async-storage/async-storage';
-import { useState,useEffect } from "react";
-import { LOAD_ITEMS,LOAD_USERS } from "../data/Reducer";
+import { useState, useEffect } from "react";
+import { LOAD_ITEMS, LOAD_USERS, UPDATE_USER } from "../data/Reducer";
 import { saveAndDispatch } from "../data/DB";
 
 
@@ -12,66 +12,93 @@ import { saveAndDispatch } from "../data/DB";
 
 
 export default function Favourites() {
- 
-  
 
+
+  //load items
   const listItems = useSelector((state) => state.listItems);
+  userId = "oFA21EeUbvSj2MyHOxnuOk3c6NE2"
 
-  //console.log(listItems,"listItems")
+  //load users
+  useEffect(() => {
+    const loadAction = { type: LOAD_USERS };
+    saveAndDispatch(loadAction, dispatch);
+  }, []);
+  const users = useSelector((state) => state.users);
+  const dispatch = useDispatch();
+  console.log(users)
+
+  //find the record of the current user 
+  const currentUser = users.filter(elem => elem.key == userId)
+  console.log(currentUser[0],"CURRENT USER")
+
+  //find favorited items from listItems
+  const favorites = listItems.filter(elem => currentUser[0].favorites.includes(elem.key))
+  console.log(favorites, "FAVORITES")
+  const [favoriteList, setFavoriteList] = useState(favorites);
+
   
- userId= "oFA21EeUbvSj2MyHOxnuOk3c6NE2"
-
-//load users
- useEffect(() => {
-  const loadAction = { type: LOAD_USERS };
-  saveAndDispatch(loadAction, dispatch);
-}, []);
-const users = useSelector((state) => state.users);
-//console.log(users)
-const dispatch = useDispatch();
-
-//find the record of the current user 
-//let currentUser = users.filter(elem => elem.uid=userId)
-//console.log(currentUser,"CURRENT USER")
- 
   return (
     <View style={styles.container}>
-       <View>
-       <FlatList
-                data={listItems}
-                renderItem={({ item }) => {
-                    return (
-                        // <HomeCard item={item}  />
-                      
+      <View>
+        <FlatList
+          data={favoriteList}
+          renderItem={({ item }) => {
+            return (
+              // <HomeCard item={item}  />
 
-                        <Card>
-                            <Card.Title>{item.name}</Card.Title>
-                            <Card.Divider />
-                             {/* <Card.Image style={{objectFit: "contain"}} source={item.imageURL} />  */}
-                             <Image style={{objectFit: "contain", width: 350, height: 200 }}source={{uri: item.imageURL}} />
-                           
-                            
-                           <View style={styles.cardBottom}>
-                           <Text style={{ marginBottom: 10 }}>
-                                {item.price}
-                            </Text>
-                            <Icon
-                                  name="heart"
-                                  type="font-awesome"
-                                  size={20}/>
 
-                           </View>
-                                
-                                
-                        </Card>
-                    );
-                }
+              <Card>
+                <Card.Title>{item.name}</Card.Title>
+                <Card.Divider />
+                {/* <Card.Image style={{objectFit: "contain"}} source={item.imageURL} />  */}
+                <Image style={{ objectFit: "contain", width: 350, height: 200 }} source={{ uri: item.imageURL }} />
 
-                }
-            />
-           
-          
-       </View>
+
+                <View style={styles.cardBottom}>
+                  <Text style={{ marginBottom: 10 }}>
+                    {item.price}
+                  </Text>
+                  <TouchableOpacity
+                    onPress={() => {
+                     const newFavorites = favoriteList.filter(elem=> elem.key !=item.key
+                     )//get only id 
+                    
+                     setFavoriteList(newFavorites)
+                     const favListDispatch = newFavorites.map(e=>e=e.key)
+                     console.log(favListDispatch)
+
+                     //console.log(newFavorites,"newFavorites")
+
+                     const action = {
+                      type: UPDATE_USER,
+                      payload: {
+                        key: currentUser[0].key,
+                        displayName: currentUser[0].displayName,
+                       favorites: favListDispatch
+                      }
+                    }
+      
+                    saveAndDispatch(action,dispatch)
+                    }}>
+                    <Icon
+                      name="heart"
+                      type="font-awesome"
+                      size={20} />
+                  </TouchableOpacity>
+
+
+                </View>
+
+
+              </Card>
+            );
+          }
+
+          }
+        />
+
+
+      </View>
     </View>
   );
 }
@@ -83,10 +110,10 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-  cardBottom:{
+  cardBottom: {
     flexDirection: 'row',
     flex: 0.3,
     justifyContent: 'space-around'
-    
+
   }
 });
